6dc51cca81ea92c8d3616056e5468e76b931f1a4
Roman Klauke <romaaan.git@gmail.com>
2015-07-13 21:38:24 +0200

    corrected comment

--- src/runtime.cc
+++ src/runtime.cc
@@ -7,17 +7,7 @@ namespace internal {
 Runtime::Runtime() {
 }
 
-/*
- * Main Runtime function. Every input buffer from the uv callback gets executed
- * in this routine.
- * The parser will build up a "pseudo" ast using the buffer input. Using the Ast
- * it's possible for the runtime to evaluate the REPS commands and execute them.
- *
- * *Note* Only "native" RESP commands are executed here. JS REPS commands are
- * executed in the recharged::internal::JSRuntime using spidermonkey.
- *
- * Runtime results are returned back to the eventloop.
- */
+
 std::string Runtime::Run(char *input) {
   Parser* parser = new Parser(input);
   Ast* ast =  parser->Parse();
